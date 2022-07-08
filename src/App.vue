b8332cf4f416cf046cdc979ef285acd2b5ddf8d8
duc tran <duceduca2022@gmail.com>
2022-07-08 18:50:08 +0700

    rename props msg ===> msg2

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld2 msg="Welcome to Your Vue.js App"/>
+    <HelloWorld2 msg2="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ msg2 }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -34,7 +34,7 @@
 export default {
   name: 'HelloWorld2',
   props: {
-    msg: String
+    msg2: String
   }
 }
 </script>
